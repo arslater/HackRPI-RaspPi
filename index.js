0f0d3427f5cbc89194665c43a91c707b20b810a1
dawneraq <dawneraq@gmail.com>
2017-11-12 06:22:28 -0500

    Make Node script read the document specified by gameId once

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('H3LL0 W012LD');
+var Firestore = require('@google-cloud/firestore');
+
+var config = {
+  projectId: 'chickenco-op-103b4',
+  keyFilename: 'chickenco-op-ed42621920d0.json'
+};
+
+// Get a reference to the database service
+var firestore = new Firestore(config);
+
+// Listen for game-start events
+var gameId = process.argv[2];
+var document = firestore.doc('games/' + gameId);
+
+console.log('Got reference to Chicken Co-Op game with ID ' + gameId + ': ' + document);
+
+// Read data once without listening for changes
+document.get()
+  .then(doc => {
+    var data = doc._fieldsProto;
+    console.log('Successfully read document: ' + JSON.stringify(data, null, 4));
+  });
